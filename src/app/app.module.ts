31e26814fd21fe8a189fa641db2131ba4e815b3f
kingrockz <kdk.dilipkumar@gmail.com>
2020-12-09 19:14:39 +0530

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { LoginComponent } from './login/login.component';
+import { Routes,RouterModule} from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from "@angular/forms";
+
+const routes : Routes = [
+
+  {path : 'login',component : LoginComponent},
+  {path : 'register',component : RegisterComponent},
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
+  exports : [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
